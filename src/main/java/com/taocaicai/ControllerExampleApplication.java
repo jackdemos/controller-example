442e125cdc9c7c46ed0c30314c01e7d6e2b67774
oakley <cqfangchaoit@sina.com>
2023-05-15 13:03:47 +0800

    update

--- src/main/java/com/taocaicai/ControllerExampleApplication.java
+++ src/main/java/com/taocaicai/ControllerExampleApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ControllerExampleApplication {
 
+    // https://juejin.cn/post/7123091045071454238
     public static void main(String[] args) {
         SpringApplication.run(ControllerExampleApplication.class, args);
     }
